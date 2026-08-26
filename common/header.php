bccd0e26c927f8cd4143e5996e891d2c284c0249
Ravi Roushan <raviroushan80@gmail.com>
2026-08-27 02:34:44 +0530

    your change description

--- common/header.php
+++ common/header.php
@@ -41,8 +41,8 @@
     <a class="shiva-mobile-follow" href="https://whatsapp.com/channel/0029VbCCeCcEVccFQmXFQ00V" target="_blank" rel="noopener"><i class="fab fa-whatsapp"></i> Follow WhatsApp Channel</a>
     <div id="menu" class="text-left"><ul class="offcanvas_main_menu"><li><a href="index.html">Home</a></li><li><a href="about.html">About</a></li><li><a href="our-product.html">Our Product</a></li><li><a href="gallery.html">Gallery</a></li><li><a href="exhibitions.html">Exhibitions & Trade Shows</a></li><li><a href="contact.html">Contact Us</a></li></ul></div>
     <div class="shiva-mobile-menu-addresses">
-      <a class="shiva-mobile-address" href="https://www.google.com/maps/search/?api=1&query=S+NO+223+CTS-2675+HISSA-687%2C+Kharodi+Marve+Road%2C+Malwani%2C+Malad+West%2C+Nr+Chheda+Complex%2C+Mumbai+400095" target="_blank" rel="noopener"><span>Office Address</span>S NO 223 CTS-2675 HISSA-687, Kharodi Marve Road, Malwani, Malad West, Nr Chheda Complex, Mumbai 400095</a>
-      <div class="shiva-mobile-address"><span>Factory Address</span>Gala No: C-2, Shree Ganesh Ind Estate, Behind Sagar Hotel, Chinchoti, Devdal, Kaman, Vasai (East) – 401208.</div>
+      <a class="shiva-mobile-address" href="https://www.google.com/maps/search/?api=1&query=Sr.No%3A223%2C+Cts-2675%2C+Hissa-687%2C+Kharodi+Marve+Road%2C+Near+Chheda+Complex%2C+Malwani%2C+Malad%28West%29%2C+Mumbai-400095" target="_blank" rel="noopener"><span>Malad Unit</span>Sr.No:223, Cts-2675, Hissa-687, Kharodi Marve Road, Near Chheda Complex, Malwani, Malad(West), Mumbai-400095</a>
+      <div class="shiva-mobile-address"><span>Vasai Unit</span>Gala No:C-2, Ganesh Ind Estate, Behind Sagar Hotel, Chinchoti, Devdal, Kaman Village, Vasai(East), Palghar-401208</div>
     </div>
   </div></div>
 </div>
